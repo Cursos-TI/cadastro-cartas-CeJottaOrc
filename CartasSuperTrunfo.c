7e97ab4f3f68a77a573674405c4dc688921e92d7
CeJottaOrc <cassioorcay76@gmail.com>
2025-09-26 05:19:56 +0000

    Completo nível aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,14 +17,14 @@ char estado1;
 char cod_carta1[20];
 char cidade1[20];
  int pop1, p_turistico1;
-float area1, pib1;
+float area1, pib1, densidade1,pibperk1;
 
 //Carta 2
 char estado2;
 char cod_carta2[20];
 char cidade2[20];
  int pop2, p_turistico2;
-float area2, pib2;
+float area2, pib2, densidade2,pibperk2;
 
 
   // Área para entrada de dados
@@ -60,20 +60,37 @@ printf("Certo! Agora vamos para a segunda carta.\n");
   printf("Quantos pontos turísticos tem essa cidade?\n");
   scanf("%i", &p_turistico2);
 
+  //Área de Operações
+densidade1 = pop1 / area1;
+densidade2 = pop2 / area2;
+pibperk1 = pib1 / pop1;
+pibperk2 = pib2 / pop2;
+
   // Área para exibição dos dados da cidade
   
   //CARTA 1
   printf("Certo! Segue as informações da CARTA N°1:\n");
   printf("Carta N°1\n");
-  printf("Estado: %c.\nCódigo: %c01.\nCidade: %s.\nPopulação: %i habitantes.\nÁrea: %.3fkm².\nPIB: %.3f bilhões de reais.\nPontos Turísticos: %i.\n"
-    ,estado1,estado1,cidade1,pop1,area1,pib1,p_turistico1);
+  printf("Estado: %c.\nCódigo: %c01.\nCidade: %s.\nPopulação: %i habitantes.\n"
+    ,estado1,estado1,cidade1,pop1);
+    printf("Área: %.3fkm².\nPIB: %.3f bilhões de reais.\nPontos Turísticos: %i.\n"
+    ,area1,pib1,p_turistico1);
+    printf("Densidade Populacional: %.2f hab/km².\nPIB per Capita %.2f reais.\n"
+      ,densidade1,pibperk1);
+    
+    
+    
 
     //CARTA 2
 
-    printf("Agora as informações da CARTA N°2:\n");
-   printf("Carta N°2\n");
-   printf("Estado: %c.\nCódigo: %c02.\nCidade: %s.\nPopulação: %i habitantes.\nÁrea: %.3fkm².\nPIB: %.3f bilhões de reais.\nPontos Turísticos: %i.\n"
-    ,estado2,estado2,cidade2,pop2,area2,pib2,p_turistico2);
+   printf("Agora as informações da CARTA N°2:\n");
+  printf("Carta N°2\n");
+  printf("Estado: %c.\nCódigo: %c02.\nCidade: %s.\nPopulação: %i habitantes.\n"
+    ,estado2,estado2,cidade2,pop2);
+    printf("Área: %.3fkm².\nPIB: %.3f bilhões de reais.\nPontos Turísticos: %i.\n"
+    ,area2,pib2,p_turistico2);
+    printf("Densidade Populacional: %.2f hab/km².\nPIB per Capita %.2f reais.\n"
+      ,densidade2,pibperk2);
 
 return 0;
 } 
